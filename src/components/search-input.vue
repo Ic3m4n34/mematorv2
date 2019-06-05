7bbc500f2273fb905bb41c394f8206682fec9f90
Nico Meyer <nico.meyer@i22.de>
2019-06-05 21:35:01 +0200

    adds searchInput

--- src/components/search-input.vue
+++ src/components/search-input.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="search-input">
-
+    <input
+      v-model="searchTerm"
+      class="search-input__input"
+    />
+    <button
+      @click="searchImages"
+      class="search-input__button"
+    />
   </div>
 </template>
 
 <script>
 export default {
   name: 'SearchInput',
-
+  data() {
+    return {
+      searchTerm: '',
+    };
+  },
+  methods: {
+    searchImages() {
+      return true;
+    },
+  },
 };
 </script>
 
--- tests/unit/search-input.spec.js
+++ tests/unit/search-input.spec.js
@@ -1,12 +1,34 @@
 import { shallowMount } from '@vue/test-utils';
 import SearchInput from '@/components/search-input';
 
-describe('SearchInput.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(SearchInput, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe('SearchInput', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(SearchInput);
+  });
+
+  it('gets rendered', () => {
+    expect(wrapper.contains('.search-input')).toBe(true);
+  });
+
+  it('input is empty', () => {
+    expect(wrapper.vm.searchTerm).toBe('');
+  });
+
+  it('v-model has data when input is given', () => {
+    const searchInput = wrapper.find('.search-input__input');
+    searchInput.value = 'images';
+
+    expect(wrapper.vm.searchTerm === 'images');
+  });
+
+  it('button triggers search', () => {
+    const stub = jest.fn();
+    const searchButton = wrapper.find('.search-input__button');
+    wrapper.setMethods({ searchImages: stub });
+
+    searchButton.trigger('click');
+    expect(wrapper.vm.searchImages).toHaveBeenCalled();
   });
 });
